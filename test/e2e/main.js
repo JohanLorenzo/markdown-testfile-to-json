17147807beeea7ab8660cd29b7f73231f13d0ef1
Johan Lorenzo <jlorenzo@mozilla.com>
2015-05-13 14:10:21 +0200

    Added unit tests to the meta-suite parser

--- test/e2e/main.js
+++ test/e2e/main.js
@@ -8,46 +8,49 @@ describe('cli', function() {
     exec("./cli.js test/e2e/default.md", function(error, stdout) {
 
       var expectedResult = JSON.stringify({
-        name: 'Launch suite',
-        testcases: [{
-          id: 'fxos.func.sanity.launch_contacts',
-          instructions: 'Launch contacts! this is an updated version',
-          state: 'draft',
-          bug: 2,
-          userStory: 1
-        }, {
-          id: 'fxos.func.sanity.launch_music',
-          instructions: 'Launch music',
-          state: 'disabled',
-          bug: 3,
-          userStory: 3
-        }, {
-          id: 'fxos.func.sanity.launch_sms_repeated',
-          instructions: 'Launch SMS and measure elapsed time\nDo it again.\nAnd again.',
-          state: 'xfail',
-          bug: 3,
-          userStory: 3
-        }, {
-          id: 'fxos.func.parameterized.test',
-          instructions: 'Launch the dialog, insert :val1, :val2, :val3 and hit enter.',
-          state: 'active',
-          variables: [{
-            ID: 'id1',
-            val1: 'a',
-            val2: '1',
-            val3: 'one one'
+        name: 'Functional',
+        suites: [{
+          name: 'Launch suite',
+          testcases: [{
+            id: 'fxos.func.sanity.launch_contacts',
+            instructions: 'Launch contacts! this is an updated version',
+            state: 'draft',
+            bug: 2,
+            userStory: 1
           }, {
-            ID: 'id2',
-            val1: 'b',
-            val2: '2',
-            val3: 'two two'
+            id: 'fxos.func.sanity.launch_music',
+            instructions: 'Launch music',
+            state: 'disabled',
+            bug: 3,
+            userStory: 3
+          }, {
+            id: 'fxos.func.sanity.launch_sms_repeated',
+            instructions: 'Launch SMS and measure elapsed time\nDo it again.\nAnd again.',
+            state: 'xfail',
+            bug: 3,
+            userStory: 3
+          }, {
+            id: 'fxos.func.parameterized.test',
+            instructions: 'Launch the dialog, insert :val1, :val2, :val3 and hit enter.',
+            state: 'active',
+            variables: [{
+              ID: 'id1',
+              val1: 'a',
+              val2: '1',
+              val3: 'one one'
+            }, {
+              ID: 'id2',
+              val1: 'b',
+              val2: '2',
+              val3: 'two two'
+            }]
+          }, {
+            id: 'fxos.func.sanity.launch_rocketbar',
+            instructions: 'Launch rocketbar',
+            state: 'draft',
+            bug: 4,
+            userStory: 3
           }]
-        }, {
-          id: 'fxos.func.sanity.launch_rocketbar',
-          instructions: 'Launch rocketbar',
-          state: 'draft',
-          bug: 4,
-          userStory: 3
         }]
       });
       expectedResult += '\n';
--- test/unit/parsers/markdown.js
+++ test/unit/parsers/markdown.js
@@ -53,46 +53,49 @@ describe('The magical ugly parser', function() {
     ].join('\n'));
 
     assert.deepEqual(actualResult, {
-      name: 'Launch suite',
-      testcases: [{
-        "id": "fxos.func.sanity.launch_contacts",
-        "bug": 2,
-        "userStory": 1,
-        "state": "draft",
-        "instructions": "Launch contacts! this is an updated version"
-      }, {
-        "id": "fxos.func.sanity.launch_music",
-        "bug": 3,
-        "userStory": 3,
-        "state": "disabled",
-        "instructions": "Launch music"
-      }, {
-        "id": "fxos.func.sanity.launch_sms_repeated",
-        "bug": 3,
-        "userStory": 3,
-        "state": "xfail",
-        "instructions": "Launch SMS and measure elapsed time\nDo it again.\nAnd again."
-      }, {
-        "id": "fxos.func.parameterized.test",
-        "instructions": "Launch the dialog, insert :val1, :val2, :val3 and hit enter.",
-        "state": "active",
-        "variables": [{
-          "ID": "id1",
-          "val1": "a",
-          "val2": "1",
-          "val3": "one one"
+      name: 'Functional',
+      suites: [{
+        name: 'Launch suite',
+        testcases: [{
+          "id": "fxos.func.sanity.launch_contacts",
+          "bug": 2,
+          "userStory": 1,
+          "state": "draft",
+          "instructions": "Launch contacts! this is an updated version"
         }, {
-          "ID": "id2",
-          "val1": "b",
-          "val2": "2",
-          "val3": "two two"
+          "id": "fxos.func.sanity.launch_music",
+          "bug": 3,
+          "userStory": 3,
+          "state": "disabled",
+          "instructions": "Launch music"
+        }, {
+          "id": "fxos.func.sanity.launch_sms_repeated",
+          "bug": 3,
+          "userStory": 3,
+          "state": "xfail",
+          "instructions": "Launch SMS and measure elapsed time\nDo it again.\nAnd again."
+        }, {
+          "id": "fxos.func.parameterized.test",
+          "instructions": "Launch the dialog, insert :val1, :val2, :val3 and hit enter.",
+          "state": "active",
+          "variables": [{
+            "ID": "id1",
+            "val1": "a",
+            "val2": "1",
+            "val3": "one one"
+          }, {
+            "ID": "id2",
+            "val1": "b",
+            "val2": "2",
+            "val3": "two two"
+          }]
+        }, {
+          "id": "fxos.func.sanity.launch_rocketbar",
+          "bug": 4,
+          "userStory": 3,
+          "state": "draft",
+          "instructions": "Launch rocketbar"
         }]
-      }, {
-        "id": "fxos.func.sanity.launch_rocketbar",
-        "bug": 4,
-        "userStory": 3,
-        "state": "draft",
-        "instructions": "Launch rocketbar"
       }]
     });
   });
